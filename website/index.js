503426b644acf36d5ba6ca904d6744fbf36eaa47
Lukas Köhler <lukas@lukkoeh.io>
2025-05-13 15:23:45 +0200

    Add zoom

--- website/index.js
+++ website/index.js
@@ -8,6 +8,18 @@ const projection = d3
   .scale(width / 1.5 / Math.PI)
   .translate([width / 2, height / 2]);
 
+const pathGenerator = d3.geoPath().projection(projection);
+
+const zoom = d3
+  .zoom()
+  .scaleExtent([1, 8])
+  .on("zoom", (event) => {
+    projection
+      .scale(event.transform.k)
+      .translate(event.transform.x, event.transform.y);
+    svg.selectAll("path").attr("d", d3.geoPath().projection(projection));
+  });
+
 // Load external data and boot
 d3.json("/geo.json").then(function(data) {
   // Draw the map
@@ -18,12 +30,12 @@ d3.json("/geo.json").then(function(data) {
     .join("path")
     .attr("fill", "#fff")
     .attr("d", d3.geoPath().projection(projection))
-    .style("stroke", "black")
+    .style("stroke", "#999999")
     .on("mouseover", (e, d) => {
       d3.select(e.currentTarget)
         .transition()
         .duration(40)
-        .attr("fill", "#7aa53d");
+        .attr("fill", "#a2c077");
     })
     .on("mouseout", (e, d) => {
       d3.select(e.currentTarget).transition().duration(40).attr("fill", "#fff");
